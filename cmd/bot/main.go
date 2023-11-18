a800799ef5ed07ec812a11e310c956666af46bfa
k-orolevsk-y <i@korolevsky.me>
2023-11-18 22:48:57 +0300

    feat: main struct (with gracefully shutdown)

--- cmd/bot/main.go
+++ cmd/bot/main.go
@@ -1,3 +1,42 @@
 package main
 
-func main() {}
+import (
+	"os"
+	"os/signal"
+	"syscall"
+
+	"go.uber.org/zap"
+
+	"github.com/k-orolevsk-y/resale-bot/internal/bot/config"
+	"github.com/k-orolevsk-y/resale-bot/pkg/bot"
+	"github.com/k-orolevsk-y/resale-bot/pkg/log"
+)
+
+func main() {
+	if err := config.ParseConfig(); err != nil {
+		panic(err)
+	}
+
+	logger, err := log.New()
+	if err != nil {
+		panic(err)
+	}
+
+	logger.Debug("initialized logger")
+	logger.Debug("parsed config", zap.Any("config", config.Config))
+
+	b, err := bot.New(logger)
+	if err != nil {
+		logger.Panic("error start bot", zap.Error(err))
+	}
+
+	b.Run()
+	logger.Info("success started bot")
+
+	quitSignal := make(chan os.Signal, 1)
+	signal.Notify(quitSignal, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
+
+	logger.Info("shutting down gracefully", zap.Any("signal", <-quitSignal))
+	b.StopReceivingUpdates()
+	logger.Info("successfully shutdown bot gracefully")
+}
