1ccfcb2dbe47326b84f91cb9fd5cea7665a38dda
k-orolevsk-y <i@korolevsky.me>
2023-11-18 22:46:30 +0300

    feat: add bot routing

---
+++ pkg/bot/constants.go
@@ -0,0 +1,18 @@
+package bot
+
+import (
+	"fmt"
+	"regexp"
+)
+
+var (
+	regexFroAllStrings = regexp.MustCompile(".*")
+
+	ErrInvalidChat            = fmt.Errorf("invalid chat")
+	ErrInvalidBigCallbackData = fmt.Errorf("invalid big callback data")
+)
+
+func defaultRecovery(ctx *Context, err interface{}) {
+	_ = ctx.Message(fmt.Sprintf("Не удалось обработать сообщение. Сообщите эту информацию разработчикам:\n%s", err))
+	ctx.Abort()
+}
