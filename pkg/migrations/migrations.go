0b3a20d60e8fe6ceedad04379f25085a83b16031
k-orolevsk-y <i@korolevsky.me>
2023-11-24 22:23:24 +0300

    feat: small work with database

--- pkg/migrations/migrations.go
+++ pkg/migrations/migrations.go
@@ -1,1 +1,28 @@
 package migrations
+
+import (
+	"embed"
+	"fmt"
+
+	"github.com/jmoiron/sqlx"
+	"github.com/pressly/goose/v3"
+)
+
+//go:embed *.sql
+var sqlMigrationFiles embed.FS
+
+func ApplyMigrations(db *sqlx.DB) error {
+	goose.SetBaseFS(sqlMigrationFiles)
+	goose.SetSequential(true)
+	goose.SetLogger(goose.NopLogger())
+
+	if err := goose.SetDialect("postgres"); err != nil {
+		return fmt.Errorf("goose.SetDialect: %w", err)
+	}
+
+	if err := goose.Up(db.DB, "."); err != nil {
+		return fmt.Errorf("goose.Up: %w", err)
+	}
+
+	return nil
+}
