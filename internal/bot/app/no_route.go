f3416d82dd6fed349e4435e8338a99667207f9de
k-orolevsk-y <i@korolevsky.me>
2023-12-02 20:31:22 +0300

    feat: add MustClearState in no route

--- internal/bot/app/no_route.go
+++ internal/bot/app/no_route.go
@@ -15,8 +15,9 @@ func (a *App) noRoute(ctx *bot.Context) {
 		if err := ctx.MessageWithKeyboard(text, keyboard); err != nil {
 			a.logger.Error("error send message (no route)", zap.Error(err))
 		}
-		ctx.Abort()
 
+		ctx.MustClearState()
+		ctx.Abort()
 	} else if ctx.Method() == "callback" {
 		ctx.AbortWithCallback(true, "Скорее всего устарела техническая информация, повторите действия.")
 	}
