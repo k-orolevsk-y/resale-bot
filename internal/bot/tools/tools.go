c0751ef9dcbc3b8052e1d21032134a2c2f484725
k-orolevsk-y <i@korolevsky.me>
2023-12-02 20:30:41 +0300

    feat: remove unused In()

--- internal/bot/tools/tools.go
+++ internal/bot/tools/tools.go
@@ -5,16 +5,6 @@ import (
 	"time"
 )
 
-func In[T comparable](val T, values []T) bool {
-	for _, v := range values {
-		if v == val {
-			return true
-		}
-	}
-
-	return false
-}
-
 func MustInt64(s string) int64 {
 	v, _ := strconv.ParseInt(s, 10, 64)
 	return v
