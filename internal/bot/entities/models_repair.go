cf64360af51a047b38ff2d8d74dbfc21c6c6a86a
k-orolevsk-y <i@korolevsky.me>
2023-12-02 20:29:08 +0300

    feat: removed entities & add fields in struct repair

--- internal/bot/entities/models_repair.go
+++
@@ -1,9 +0,0 @@
-package entities
-
-import "github.com/google/uuid"
-
-type ModelRepair struct {
-	ID               uuid.UUID `db:"id"`
-	CategoryRepairID uuid.UUID `db:"category_repair_id"`
-	Name             string    `db:"name"`
-}
