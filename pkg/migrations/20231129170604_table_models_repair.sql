ce2eb3e904d971a885a64b8ee55884487c8237d0
k-orolevsk-y <i@korolevsky.me>
2023-12-02 20:28:00 +0300

    build: update migrations

--- pkg/migrations/20231129170604_table_models_repair.sql
+++
@@ -1,7 +0,0 @@
--- +goose Up
-    CREATE TABLE IF NOT EXISTS models_repair (
-        id uuid default gen_random_uuid(),
-        category_repair_id uuid not null,
-        name varchar(64) not null
-    );
--- +goose Down
--- pkg/migrations/20231129170756_table_reapir.sql
+++ pkg/migrations/20231129170756_table_reapir.sql
@@ -1,7 +1,8 @@
 -- +goose Up
     CREATE TABLE IF NOT EXISTS repair (
         id uuid default gen_random_uuid(),
-        model_id uuid not null,
+        producer_name varchar(64) not null,
+        model_name varchar(64) not null,
         name varchar(64) not null,
         description text,
         price double precision not null default 0.0
