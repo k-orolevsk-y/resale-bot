8ce6a7cfc9cfc5d9657ff5f7e518f420d541a2a4
k-orolevsk-y <i@korolevsky.me>
2023-12-02 20:29:36 +0300

    feat: add functions in repository postgres

--- internal/bot/repository/postgres/repair.go
+++ internal/bot/repository/postgres/repair.go
@@ -9,10 +9,59 @@ import (
 	"github.com/k-orolevsk-y/resale-bot/internal/bot/entities"
 )
 
+func (pg *Pg) CreateRepair(ctx context.Context, repair *entities.Repair) error {
+	query := "INSERT INTO repair (producer_name, model_name, name, description, price) VALUES ($1, $2, $3, $4, $5)"
+	id, err := pg.db.ExecContextWithReturnID(ctx, query, repair.ProducerName, repair.ModelName, repair.Name, repair.Description, repair.Price)
+
+	if err != nil {
+		return err
+	}
+
+	repair.ID = uuid.MustParse(id.(string))
+	return nil
+}
+
+func (pg *Pg) EditRepair(ctx context.Context, repair *entities.Repair) error {
+	query := "UPDATE repair SET producer_name = $1, model_name = $2, name = $3, description = $4, price = $5 WHERE id = $6"
+	_, err := pg.db.ExecContext(ctx, query, repair.ProducerName, repair.ModelName, repair.Name, repair.Description, repair.Price, repair.ID)
+
+	return err
+}
+
+func (pg *Pg) GetProducersRepair(ctx context.Context) ([]string, error) {
+	var categories []string
+
+	query := "SELECT DISTINCT(repair.producer_name) FROM repair"
+	err := pg.db.SelectContext(ctx, &categories, query)
+
+	if err != nil {
+		return nil, err
+	} else if len(categories) < 1 {
+		return nil, sql.ErrNoRows
+	}
+
+	return categories, nil
+}
+
+func (pg *Pg) GetModelsRepair(ctx context.Context, producerName string) ([]string, error) {
+	var models []string
+
+	query := "SELECT DISTINCT(repair.model_name) FROM repair WHERE lower(producer_name) = lower($1)"
+	err := pg.db.SelectContext(ctx, &models, query, producerName)
+
+	if err != nil {
+		return nil, err
+	} else if len(models) < 1 {
+		return nil, sql.ErrNoRows
+	}
+
+	return models, nil
+}
+
 func (pg *Pg) GetRepairs(ctx context.Context, modelName string) ([]entities.Repair, error) {
 	var repairs []entities.Repair
 
-	query := "SELECT repair.* FROM models_repair JOIN repair ON models_repair.id = repair.model_id WHERE lower(models_repair.name) = lower($1)"
+	query := "SELECT * FROM repair WHERE lower(model_name) = lower($1)"
 	err := pg.db.SelectContext(ctx, &repairs, query, modelName)
 
 	if err != nil {
@@ -24,44 +73,45 @@ func (pg *Pg) GetRepairs(ctx context.Context, modelName string) ([]entities.Repa
 	return repairs, nil
 }
 
-func (pg *Pg) GetRepairWithModelAndCategory(ctx context.Context, modelID uuid.UUID, repairName string) (*entities.RepairWithModelAndCategory, error) {
-	var repair entities.RepairWithModelAndCategory
+func (pg *Pg) GetAllRepairs(ctx context.Context) ([]entities.Repair, error) {
+	var repairs []entities.Repair
 
-	query := `
-		SELECT
-			repair.*, models_repair.name AS model_name, categories_repair.name as category_name
-		FROM models_repair
-			JOIN repair
-				ON models_repair.id = repair.model_id
-			JOIN categories_repair
-				ON categories_repair.id = models_repair.category_repair_id
-		WHERE
-			models_repair.id = $1
-		  AND
-			lower(repair.name) = lower($2)
-	`
-	err := pg.db.GetContext(ctx, &repair, query, modelID, repairName)
+	query := "SELECT * FROM repair"
+	err := pg.db.SelectContext(ctx, &repairs, query)
+
+	return repairs, err
+}
+
+func (pg *Pg) GetRepairByModelAndName(ctx context.Context, modelName string, repairName string) (*entities.Repair, error) {
+	var repair entities.Repair
+
+	query := "SELECT * FROM repair WHERE lower(model_name) = lower($1) AND lower(name) = lower($2)"
+	err := pg.db.GetContext(ctx, &repair, query, modelName, repairName)
 
 	return &repair, err
 }
 
-func (pg *Pg) GetRepairWithModelAndCategoryByID(ctx context.Context, modelID, repairID uuid.UUID) (*entities.RepairWithModelAndCategory, error) {
-	var repair entities.RepairWithModelAndCategory
+func (pg *Pg) GetRepairByModelAndID(ctx context.Context, modelName string, id uuid.UUID) (*entities.Repair, error) {
+	var repair entities.Repair
 
-	query := `
-		SELECT
-			repair.*, models_repair.name AS model_name, categories_repair.name as category_name
-		FROM models_repair
-			JOIN repair
-				ON models_repair.id = repair.model_id
-			JOIN categories_repair
-				ON categories_repair.id = models_repair.category_repair_id
-		WHERE
-			models_repair.id = $1
-		  AND
-			repair.id = $2
-	`
-	err := pg.db.GetContext(ctx, &repair, query, modelID, repairID)
+	query := "SELECT * FROM repair WHERE lower(model_name) = lower($1) AND id = $2"
+	err := pg.db.GetContext(ctx, &repair, query, modelName, id)
 
 	return &repair, err
+}
+
+func (pg *Pg) GetRepairByID(ctx context.Context, id uuid.UUID) (*entities.Repair, error) {
+	var repair entities.Repair
+
+	query := "SELECT * FROM repair WHERE id = $1"
+	err := pg.db.GetContext(ctx, &repair, query, id)
+
+	return &repair, err
+}
+
+func (pg *Pg) DeleteRepairByID(ctx context.Context, id uuid.UUID) error {
+	query := "DELETE FROM repair WHERE id = $1"
+	_, err := pg.db.ExecContext(ctx, query, id)
+
+	return err
 }
